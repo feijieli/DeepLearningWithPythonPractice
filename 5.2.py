555ae640fafb90feb34c7d27c9cbac9142a313a5
feijieli <lee12064@gmail.com>
2020-11-05 21:03:17 +0800

    finished chapter5

---
+++ 5.2.py
@@ -0,0 +1,132 @@
+# %% prepare the data
+import os,shutil
+
+original_dataset_dir = 'dogs-vs-cats/train'
+base_dir='cats_and_dogs_small'
+train_dir=os.path.join(base_dir,'train')
+validation_dir=os.path.join(base_dir,'validation')
+test_dir=os.path.join(base_dir,'test')
+train_cats_dir= os.path.join(train_dir,'cats')
+train_dogs_dir= os.path.join(train_dir,'dogs')
+validation_cats_dir= os.path.join(validation_dir,'cats')
+validation_dogs_dir= os.path.join(validation_dir,'dogs')
+test_cats_dir= os.path.join(test_dir,'cats')
+test_dogs_dir= os.path.join(test_dir,'dogs')
+
+
+#%% 
+os.mkdir(base_dir)
+os.mkdir(train_dir)
+os.mkdir(validation_dir)
+os.mkdir(test_dir)
+os.mkdir(train_cats_dir)
+os.mkdir(train_dogs_dir)
+os.mkdir(validation_cats_dir)
+os.mkdir(validation_dogs_dir)
+os.mkdir(test_cats_dir)
+os.mkdir(test_dogs_dir)
+
+for fname in ['cat.{}.jpg'.format(i) for i in range(1000)]:
+    src = os.path.join(original_dataset_dir,fname)
+    dst = os.path.join(train_cats_dir,fname)
+    shutil.copyfile(src,dst)
+
+for fname in ['cat.{}.jpg'.format(i) for i in range(1000,1500)]:
+    src = os.path.join(original_dataset_dir,fname)
+    dst = os.path.join(validation_cats_dir,fname)
+    shutil.copyfile(src,dst)
+
+for fname in ['cat.{}.jpg'.format(i) for i in range(1500,2000)]:
+    src = os.path.join(original_dataset_dir,fname)
+    dst = os.path.join(test_cats_dir,fname)
+    shutil.copyfile(src,dst)
+
+for fname in ['dog.{}.jpg'.format(i) for i in range(1000)]:
+    src = os.path.join(original_dataset_dir,fname)
+    dst = os.path.join(train_dogs_dir,fname)
+    shutil.copyfile(src,dst)
+for fname in ['dog.{}.jpg'.format(i) for i in range(1000,1500)]:
+    src = os.path.join(original_dataset_dir,fname)
+    dst = os.path.join(validation_dogs_dir,fname)
+    shutil.copyfile(src,dst)
+for fname in ['dog.{}.jpg'.format(i) for i in range(1500,2000)]:
+    src = os.path.join(original_dataset_dir,fname)
+    dst = os.path.join(test_dogs_dir,fname)
+    shutil.copyfile(src,dst)
+    
+# %% 
+from tensorflow.keras import layers, models
+from tensorflow.keras import optimizers
+
+model = models.Sequential()
+model.add(layers.Conv2D(32,(3,3),activation='relu',input_shape=(150,150,3)))
+model.add(layers.MaxPool2D((2,2)))
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+model.add(layers.MaxPool2D((2,2)))
+model.add(layers.Conv2D(128,(3,3),activation='relu'))
+model.add(layers.MaxPool2D((2,2)))
+model.add(layers.Conv2D(128,(3,3),activation='relu'))
+model.add(layers.MaxPool2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512,activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid'))
+
+model.compile(optimizer=optimizers.RMSprop(lr=1e-4),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+# %%
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150,150),
+    batch_size=20,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150,150),
+    batch_size=20,
+    class_mode='binary')
+
+#%%
+history = model.fit(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=10,
+    validation_data=validation_generator,
+    validation_steps=50
+)
+
+model.save('cats_and_dogs_small_1.h5')
+
+# %%
+import matplotlib.pyplot as plt
+
+history_dict = history.history
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1,len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label = 'Training acc')
+plt.plot(epochs, val_acc, 'b', label = 'Validation acc')
+plt.title('Training and validation acc')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label = 'Training loss')
+plt.plot(epochs, val_loss, 'b', label = 'Validation loss')
+plt.title('Training and validation acc')
+plt.legend()
+
+plt.show()
+
+
+# %%
